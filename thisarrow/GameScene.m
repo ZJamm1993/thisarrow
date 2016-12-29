5ab42f8459a45f27e8e1863897abb2fb98b595a7
jam <jam@jamdeMacBook-Pro.local>
2016-12-29 22:39:30 +0800

    return while call update: if paused

--- thisarrow/GameScene.m
+++ thisarrow/GameScene.m
@@ -165,6 +165,10 @@ const NSInteger maxDotCount=200;
 
 -(void)update:(CFTimeInterval)currentTime {
     
+    if (self.paused) {
+        return;
+    }
+    
     if ([_motionManager isAccelerometerAvailable]) {
         [arrow actionWithAcceleration:_motionManager.accelerometerData.acceleration];
     }
